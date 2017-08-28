f628da3f930139a423e1c487f0dec579ec5523ef
taiyan33 <taiyan33@users.noreply.github.com>
2017-08-29 00:44:01 +0800

    Add files via upload

--- hw1.java
+++ hw1.java
@@ -84,3 +84,26 @@ public class hw1 {
     }
 }
 
+
+// import java.math.*;
+// import java.lang.*;
+//// BigDecimalDemo
+// public class hw2 {
+
+   // public static void main(String[] args) {
+
+      ////create 3 BigDecimal objects
+      // BigDecimal bg1, bg2, bg3;
+
+      // bg1 = new BigDecimal("15");
+      // bg2 = new BigDecimal("2");
+
+      ////divide bg1 with bg2 with 3 scale
+      // bg3 = bg1.divide(bg2, 3, RoundingMode.HALF_UP);
+
+      // String str = "Division result is " +bg3;
+
+      ////print bg3 value
+      // System.out.println( str );
+   // }
+// }
